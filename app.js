263b18caf915f5a5df4beaab8a456cb4ab04a4ae
Yael Castro <cyaelcc94@gmail.com>
2019-09-02 21:32:23 -0500

    end chapter3

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 new Vue ({
     el:"#vue-app",
     data: {
-        name: "Yael"
+        name: "Yael",
+        job: "Programmer"
+    },
+    methods: {
+        greet: function(time){
+            return "Good " + time + " " + this.name
+        }
     }
 })
